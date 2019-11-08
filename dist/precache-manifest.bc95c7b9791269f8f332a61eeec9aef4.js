dc83239fc16f3173490ad8d1327df031714d019e
gladysefirina <glady.sefirina@kawancicil.co.id>
2019-11-08 16:07:28 +0700

    edit file structure

---
+++ dist/precache-manifest.bc95c7b9791269f8f332a61eeec9aef4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb385ff1ca011d00668b",
+    "url": "/Frontend-test-fabelio/css/app.7c84fe49.css"
+  },
+  {
+    "revision": "4cb246f6b810622fa9f6e7f98b910c74",
+    "url": "/Frontend-test-fabelio/index.html"
+  },
+  {
+    "revision": "869dd6fd0eeee795e0a7",
+    "url": "/Frontend-test-fabelio/js/about.da71186f.js"
+  },
+  {
+    "revision": "fb385ff1ca011d00668b",
+    "url": "/Frontend-test-fabelio/js/app.8a6384af.js"
+  },
+  {
+    "revision": "ce49a6e43e934168307d",
+    "url": "/Frontend-test-fabelio/js/chunk-vendors.c05f44ac.js"
+  },
+  {
+    "revision": "a3b77eeb6a16462cb025d0ed4ce244a7",
+    "url": "/Frontend-test-fabelio/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Frontend-test-fabelio/robots.txt"
+  }
+]);
---
+++ vue.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    publicPath: '/Frontend-test-fabelio/'
+}
